999ecd311b646613c9e74e4fd756dbfa86a967db
JOSHmax1406 <38108222+JOSHmax1406@users.noreply.github.com>
2018-04-06 00:22:57 +0700

    joshmax

---
+++ myscript.js
@@ -0,0 +1,90 @@
+$(document).ready(function()
+{
+    $("#button-popup").click(function()
+    {
+        $(".window-popup").slideToggle(300);
+    });
+
+    $("#button-popup-close").click(function()
+    {
+        $(".window-popup").fadeOut(300);
+    });
+});
+
+$(document).ready(function()
+{
+    $("#button-popup1").click(function()
+    {
+        $(".window-popup1").slideToggle(300);
+    });
+
+    $("#button-popup-close1").click(function()
+    {
+        $(".window-popup1").fadeOut(300);
+    });
+});
+
+$(document).ready(function()
+{
+    $("#button-popup2").click(function()
+    {
+        $(".window-popup2").slideToggle(300);
+    });
+
+    $("#button-popup-close2").click(function()
+    {
+        $(".window-popup2").fadeOut(300);
+    });
+});
+
+$(document).ready(function()
+{
+    $("#button-popup3").click(function()
+    {
+        $(".window-popup3").slideToggle(300);
+    });
+
+    $("#button-popup-close3").click(function()
+    {
+        $(".window-popup3").fadeOut(300);
+    });
+});
+
+$(document).ready(function()
+{
+    $("#button-popup4").click(function()
+    {
+        $(".window-popup4").slideToggle(300);
+    });
+
+    $("#button-popup-close4").click(function()
+    {
+        $(".window-popup4").fadeOut(300);
+    });
+});
+
+$(document).ready(function()
+{
+    $("#button-popup5").click(function()
+    {
+        $(".window-popup5").slideToggle(300);
+    });
+
+    $("#button-popup-close5").click(function()
+    {
+        $(".window-popup5").fadeOut(300);
+    });
+});
+
+$(document).ready(function()
+{
+    $("#button-popup6").click(function()
+    {
+        $(".window-popup6").slideToggle(300);
+    });
+
+    $("#button-popup-close6").click(function()
+    {
+        $(".window-popup6").fadeOut(300);
+    });
+});
